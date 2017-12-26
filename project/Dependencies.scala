411a7d09c3a4dfc007217285aa5b99e10d1b19c9
Rodrigo Fernandes <rodrigo.fernandes@tecnico.ulisboa.pt>
2017-12-26 13:15:08 +0000

    Update codacy-scala-api to version 3.0.3 (#67)
    * Update codacy-scala-api to version 3.0.3
    
    * Update coverage-parser to version 2.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
 
-  lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.1"
-  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.1"
+  lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.3"
+  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.3"
   lazy val scopt = "com.github.scopt" %% "scopt" % "3.3.0"
   lazy val log = "ch.qos.logback" % "logback-classic" % "1.2.1"
   lazy val raptureJsonPlay = "com.propensive" %% "rapture-json-play" % "2.0.0-M7"
