685e2fa0727779180b1e8657d37e72776680ea2a
Daniel Reigada <daniel@codacy.com>
2018-02-27 16:18:21 +0000

    Move config parser to configuration rules

--- src/main/scala/com/codacy/CodacyCoverageReporter.scala
+++ src/main/scala/com/codacy/CodacyCoverageReporter.scala
@@ -1,13 +1,10 @@
 package com.codacy
 
-import java.io.File
 import java.net.URL
 
 import ch.qos.logback.classic.{Level, Logger}
-import com.codacy.model.configuration.Config
 import com.codacy.rules.{ConfigurationRules, ReportRules}
 import org.slf4j.LoggerFactory
-import scopt.OptionParser
 
 import scala.util.Try
 
@@ -22,15 +19,9 @@ object CodacyCoverageReporter {
   private[codacy] lazy val configRules = new ConfigurationRules(logger)
 
 
-  private def validUrl(baseUrl: String) = {
-    Try(new URL(baseUrl)).toOption.isDefined
-  }
-
   def main(args: Array[String]): Unit = {
 
-    val parser = buildParser
-
-    parser.parse(args, configRules.emptyConfig) match {
+    configRules.parseConfig(args) match {
       case Some(config) if !validUrl(config.codacyApiBaseUrl) =>
         logger.error(s"Error: Invalid CODACY_API_BASE_URL: ${config.codacyApiBaseUrl}")
         if (!config.codacyApiBaseUrl.startsWith("http")) {
@@ -54,35 +45,8 @@ object CodacyCoverageReporter {
 
   }
 
-  def buildParser: OptionParser[Config] = {
-    new scopt.OptionParser[Config]("codacy-coverage-reporter") {
-      head("codacy-coverage-reporter", getClass.getPackage.getImplementationVersion)
-      opt[String]('l', "language").required().action { (x, c) =>
-        c.copy(languageStr = x)
-      }.text("your project language")
-      opt[Unit]('f', "forceLanguage").optional().hidden().action { (_, c) =>
-        c.copy(forceLanguage = true)
-      }
-      opt[String]('t', "projectToken").optional().action { (x, c) =>
-        c.copy(projectToken = x)
-      }.text("your project API token")
-      opt[File]('r', "coverageReport").required().action { (x, c) =>
-        c.copy(coverageReport = x)
-      }.text("your project coverage file name")
-      opt[String]("codacyApiBaseUrl").optional().action { (x, c) =>
-        c.copy(codacyApiBaseUrl = x)
-      }.text("the base URL for the Codacy API")
-      opt[String]("prefix").optional().action { (x, c) =>
-        c.copy(prefix = x)
-      }.text("the project path prefix")
-      opt[String]("commitUUID").optional().action { (x, c) =>
-        c.copy(commitUUID = Some(x))
-      }.text("your commitUUID")
-      opt[Unit]("debug").optional().hidden().action { (_, c) =>
-        c.copy(debug = true)
-      }
-      help("help").text("prints this usage text")
-    }
+  private def validUrl(baseUrl: String) = {
+    Try(new URL(baseUrl)).toOption.isDefined
   }
 
 }
--- src/main/scala/com/codacy/rules/ConfigurationRules.scala
+++ src/main/scala/com/codacy/rules/ConfigurationRules.scala
@@ -3,38 +3,61 @@ package com.codacy.rules
 import java.io.File
 
 import ch.qos.logback.classic.Logger
+import com.codacy.CodacyCoverageReporter.configRules
 import com.codacy.api.Language
 import com.codacy.model.configuration.Config
+import scopt.OptionParser
 
 class ConfigurationRules(logger: => Logger) {
+  private val publicApiBaseUrl = "https://api.codacy.com"
 
   val emptyConfig =
     Config(
       Language.Python.toString,
       forceLanguage = false,
-      ConfigurationRules.getProjectToken,
+      getProjectToken,
       new File("coverage.xml"),
-      ConfigurationRules.getApiBaseUrl,
+      getApiBaseUrl,
       "",
-      ConfigurationRules.commitUUIDOpt,
+      commitUUIDOpt,
       debug = false)
 
-}
 
+  def parseConfig(args: Array[String]): Option[Config] =
+    buildParser.parse(args, configRules.emptyConfig)
 
-object ConfigurationRules {
-
-  private val publicApiBaseUrl = "https://api.codacy.com"
-
-  private def getApiBaseUrl: String = {
-    sys.env.getOrElse("CODACY_API_BASE_URL", publicApiBaseUrl)
+  private def buildParser: OptionParser[Config] = {
+    new scopt.OptionParser[Config]("codacy-coverage-reporter") {
+      head("codacy-coverage-reporter", getClass.getPackage.getImplementationVersion)
+      opt[String]('l', "language").required().action { (x, c) =>
+        c.copy(languageStr = x)
+      }.text("your project language")
+      opt[Unit]('f', "forceLanguage").optional().hidden().action { (_, c) =>
+        c.copy(forceLanguage = true)
+      }
+      opt[String]('t', "projectToken").optional().action { (x, c) =>
+        c.copy(projectToken = x)
+      }.text("your project API token")
+      opt[File]('r', "coverageReport").required().action { (x, c) =>
+        c.copy(coverageReport = x)
+      }.text("your project coverage file name")
+      opt[String]("codacyApiBaseUrl").optional().action { (x, c) =>
+        c.copy(codacyApiBaseUrl = x)
+      }.text("the base URL for the Codacy API")
+      opt[String]("prefix").optional().action { (x, c) =>
+        c.copy(prefix = x)
+      }.text("the project path prefix")
+      opt[String]("commitUUID").optional().action { (x, c) =>
+        c.copy(commitUUID = Some(x))
+      }.text("your commitUUID")
+      opt[Unit]("debug").optional().hidden().action { (_, c) =>
+        c.copy(debug = true)
+      }
+      help("help").text("prints this usage text")
+    }
   }
 
-  private def getProjectToken: String = {
-    sys.env.getOrElse("CODACY_PROJECT_TOKEN", "")
-  }
-
-  private lazy val commitUUIDOpt: Option[String] = {
+  private def commitUUIDOpt: Option[String] = {
     getNonEmptyEnv("CI_COMMIT") orElse
       getNonEmptyEnv("TRAVIS_PULL_REQUEST_SHA") orElse
       getNonEmptyEnv("TRAVIS_COMMIT") orElse
@@ -48,6 +71,14 @@ object ConfigurationRules {
 
   private def getNonEmptyEnv(key: String): Option[String] = {
     sys.env.get(key).filter(_.trim.nonEmpty)
+  }
+
+  private def getApiBaseUrl: String = {
+    sys.env.getOrElse("CODACY_API_BASE_URL", publicApiBaseUrl)
+  }
+
+  private def getProjectToken: String = {
+    sys.env.getOrElse("CODACY_PROJECT_TOKEN", "")
   }
 
 }
