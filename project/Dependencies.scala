775cae2634c09cb995a513c38ac46ac4e91e75c6
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-07-23 12:12:50 +0100

    Add javax activation

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,17 @@ import sbt._
 
 object Dependencies {
 
-  lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.7"
-  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.9"
-  lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "1.2.0"
-  lazy val raptureJsonPlay = "com.propensive" %% "rapture-json-play" % "2.0.0-M8"
+  val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.7"
+  val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.9"
+  val caseApp = "com.github.alexarchambault" %% "case-app" % "1.2.0"
+  val raptureJsonPlay = "com.propensive" %% "rapture-json-play" % "2.0.0-M8"
+  val javaxActivation = "com.sun.activation" % "javax.activation" % "1.2.0"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 
-  lazy val cats = "org.typelevel" %% "cats-core" % "1.0.1"
+  val cats = "org.typelevel" %% "cats-core" % "1.0.1"
 
-  lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
-  lazy val log4s = "org.log4s" %% "log4s" % "1.5.0"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  val log4s = "org.log4s" %% "log4s" % "1.5.0"
+
 }
