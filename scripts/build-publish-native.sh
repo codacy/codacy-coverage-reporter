5c03c0abd5d47738b640aafd72927becf1b6d3b9
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-03-11 17:15:24 +0000

    clean: Compression using UPX on CI

--- scripts/build-publish-native.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sbt clean
-sbt 'show graalvm-native-image:packageBin'
-docker build -t compressor:latest .
-docker run --rm=true -it --user=root --entrypoint=bash -v $PWD/target/graalvm-native-image/:/opt/app/ compressor:latest -c "upx --lzma codacy-coverage-reporter -o codacy-coverage-reporter-compressed"
-mv $PWD/target/graalvm-native-image/codacy-coverage-reporter-compressed codacy-coverage-reporter-linux-$(cat .version)
