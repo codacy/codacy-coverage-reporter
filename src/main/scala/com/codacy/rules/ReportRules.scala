b06854009ca7ebc886ad8af98c0c16726fa47a12
Francisco Duarte <francisco@codacy.com>
2020-06-22 10:40:05 +0100

    fix: Fix info message wording
    Co-authored-by: Paulo Ribeiro <60105800+pauloribeiro-codacy@users.noreply.github.com>

--- src/main/scala/com/codacy/rules/ReportRules.scala
+++ src/main/scala/com/codacy/rules/ReportRules.scala
@@ -74,7 +74,7 @@ class ReportRules(coverageServices: => CoverageServices) extends LogSupport {
       }
       if (config.partial) {
         logger.info(
-          """To complete the reporting process, call the coverage-reporter with a final flag.
+          """To complete the reporting process, call coverage-reporter with the final flag.
           | Check https://github.com/codacy/codacy-coverage-reporter#multiple-coverage-reports-for-the-same-language
           | for more information.""".stripMargin
         )
