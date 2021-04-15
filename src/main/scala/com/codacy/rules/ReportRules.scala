84068f19e850957817aa15de263341d3f5d68500
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2021-04-15 13:57:42 +0100

    fix: Update documentation url
    Co-authored-by: Paulo Ribeiro <paulo.ribeiro@codacy.com>

--- src/main/scala/com/codacy/rules/ReportRules.scala
+++ src/main/scala/com/codacy/rules/ReportRules.scala
@@ -79,11 +79,9 @@ class ReportRules(coverageServices: => CoverageServices) extends LogSupport {
         }
       }
       if (config.partial) {
-        logger.info(
-          """To complete the reporting process, call coverage-reporter with the final flag.
-            | Check https://docs.codacy.com/coverage-reporter/adding-coverage-to-your-repository/#multiple-reports
-            | for more information.""".stripMargin
-        )
+        logger.info("""To complete the reporting process, call coverage-reporter with the final flag.
+            | Check https://docs.codacy.com/coverage-reporter/#multiple-reports
+            | for more information.""".stripMargin)
       }
       operationResult
     }
