3f283a3051ea35c62bfa6f4fb954fc18fba895ee
Stefan Vacareanu <stefan@codacy.com>
2022-07-06 20:20:39 +0300

    Add retry method when sending coverage v1

--- src/main/scala/com/codacy/CodacyCoverageReporter.scala
+++ src/main/scala/com/codacy/CodacyCoverageReporter.scala
@@ -16,16 +16,37 @@ object CodacyCoverageReporter extends ConfigurationParsingApp with LogSupport {
       logger.info("Skip reporting coverage")
       0
     } else {
+      val sleepTime = 10 // seconds
+      val retryTimes = 3 // total number of sending coverage attempts
+      retryReport(sleepTime, currentRetry = 1, retryTimes)(commandConfig, sys.env)
+      1
+    }
+  }
+
+  private def retryReport(sleepTime: Int, currentRetry: Int, retryTimes: Int)(
+      commandConfig: CommandConfiguration,
+      envVars: Map[String, String]
+  ): AnyVal = {
+    if (currentRetry <= retryTimes) {
+      if (currentRetry > 1) {
+        logger.info("Sleeping " + sleepTime + " seconds...")
+        Thread.sleep(sleepTime * 1000)
+      }
       val result: Either[String, String] = sendReport(commandConfig, sys.env)
       result match {
+        case Left(message) =>
+          logger.error("Attempt no." + currentRetry + " of sending coverage failed: " + message)
+          retryReport(sleepTime, currentRetry + 1, retryTimes)(commandConfig, sys.env)
+          1
         case Right(message) =>
           logger.info(message)
           0
-        case Left(message) =>
-          logger.error(message)
-          1
       }
+    } else {
+      logger.info("Attempting to send coverage failed.")
+      1
     }
+
   }
 
   private def sendReport(commandConfig: CommandConfiguration, envVars: Map[String, String]) = {
