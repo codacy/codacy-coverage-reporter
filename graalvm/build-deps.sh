0894e9508e8d902c35ac4291418b944fda1adc26
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-11-28 19:28:50 +0100

    Fix build-deps.sh

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -2,12 +2,12 @@
 
 set -e +o pipefail
 
-export TOOLCHAIN_DIR=$HOME/.musl
-mkdir -p $TOOLCHAIN_DIR
-cd $TOOLCHAIN_DIR
+mkdir -p $HOME/.musl
+cd $HOME/.musl
 
 curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
 tar -xf musl.tgz
+TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
 export CC=$TOOLCHAIN_DIR/bin/gcc
 
 zlib='zlib-1.2.13'
@@ -21,3 +21,5 @@ tar -xf $zlibtargz
   make -j "$(nproc)"
   sudo make install
 )
+
+echo "$TOOLCHAIN_DIR/bin"
