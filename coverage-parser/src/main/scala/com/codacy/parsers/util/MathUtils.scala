15df687446ca6bc3d13bc894dc6119c023ae316f
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-11-07 16:53:17 +0100

    fix: Safe sum with high numbers
    - Add test with big hits number

--- coverage-parser/src/main/scala/com/codacy/parsers/util/MathUtils.scala
+++ coverage-parser/src/main/scala/com/codacy/parsers/util/MathUtils.scala
@@ -7,12 +7,13 @@ object MathUtils {
   }
 
   implicit class ParseIntOps(val s: String) extends AnyVal {
+    def toIntOrMaxValue: Int = BigInt(s).toIntOrMaxValue
+  }
 
-    def toIntOrMaxValue: Int = {
-      val bigInt = BigInt(s)
+  implicit class BigIntOps(val bigInt: BigInt) extends AnyVal {
 
+    def toIntOrMaxValue: Int =
       if (bigInt.isValidInt) bigInt.toInt
       else Int.MaxValue
-    }
   }
 }
