af29ed5f959c59af32dfee4b8e2b8b8b6b8b70b4
Hélio Rocha <hjrocha@gmail.com>
2024-07-23 11:37:45 +0100

    bump codacy base orb

--- get.sh
+++ get.sh
@@ -129,7 +129,7 @@ download() {
 
     download_file "$url"
     checksum "$file_name" "$checksum_url"
-    if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin x86_64" ]; then
+    if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin arm64" ]; then
         mv "$file_name" "$output_filename"
     fi
 
@@ -137,7 +137,7 @@ download() {
 }
 
 download_reporter() {
-    if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin x86_64" ]; then
+    if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin arm64" ]; then
         # OS name lower case
         suffix=$(echo "$os_name" | tr '[:upper:]' '[:lower:]')
     else
@@ -203,7 +203,7 @@ if [ -z "$CODACY_REPORTER_TMP_FOLDER" ]; then
 fi
 
 # Set binary name
-if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin x86_64" ]; then
+if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin arm64" ]; then
     reporter_filename="codacy-coverage-reporter"
 else
     reporter_filename="codacy-coverage-reporter-assembly.jar"
@@ -220,7 +220,7 @@ reporter_path="$reporter_folder"/"$reporter_filename"
 
 download_reporter "$reporter_path" "$reporter_folder" "$reporter_filename"
 
-if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin x86_64" ]; then
+if [ "$os_name_arch" = "Linux x86_64" ] || [ "$os_name_arch" = "Darwin arm64" ]; then
     chmod +x "$reporter_path"
     run_command="$reporter_path"
 else
