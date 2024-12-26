8769aa5a33d9070664e61f4438714028a36250f0
david.marinho@codacy.com <david.marinho@codacy.com>
2024-12-26 17:26:20 +0000

    run scalafmt

--- src/main/scala/com/codacy/transformation/GitFileNameUpdaterAndFilter.scala
+++ src/main/scala/com/codacy/transformation/GitFileNameUpdaterAndFilter.scala
@@ -8,8 +8,8 @@ class GitFileNameUpdaterAndFilter(acceptableFileNamesMap: Map[String, Seq[String
     with LogSupport {
   override def execute(report: CoverageReport): CoverageReport = {
     val allFiles = acceptableFileNamesMap.values.flatten.toList
-      logger
-        .info(s"Files: $allFiles")
+    logger
+      .info(s"Files: $allFiles")
     val fileReports = for {
       fileReport <- report.fileReports
       fileName <- matchAndReturnName(fileReport.filename)
