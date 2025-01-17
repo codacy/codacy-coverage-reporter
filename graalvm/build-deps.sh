d4e17d0715920141005a79cf13282e78f488f12b
Leandro Salgado <lmocs98@gmail.com>
2025-01-17 12:57:57 +0000

    infra: Add suport for building Linux ARM binaries

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -5,9 +5,16 @@ set -e +o pipefail
 mkdir -p $HOME/.musl
 cd $HOME/.musl
 
-curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
+if [$ARCH == "arm"];
+then
+  curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz
+  TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
+else
+  curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
+  TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
+fi
 tar -xf musl.tgz
-TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
+
 export CC=$TOOLCHAIN_DIR/bin/gcc
 
 zlib='zlib-1.2.13'
