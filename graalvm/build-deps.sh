7fdda9a1998d31515e62ede3b80e6f50e4c7c87e
Leandro Salgado <lmocs98@gmail.com>
2025-01-21 11:08:38 +0000

    fix comparison in shell script

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -5,7 +5,7 @@ set -e +o pipefail
 mkdir -p $HOME/.musl
 cd $HOME/.musl
 
-if [$ARCH == "arm"];
+if [ "$ARCH" == "arm" ];
 then
   curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
@@ -13,20 +13,3 @@ else
   curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
 fi
-tar -xf musl.tgz
-
-export CC=$TOOLCHAIN_DIR/bin/gcc
-
-zlib='zlib-1.2.13'
-zlibtargz=$zlib.tar.gz
-curl https://zlib.net/fossils/$zlibtargz --output $zlibtargz
-tar -xf $zlibtargz
-
-(
-  cd $zlib
-  ./configure --prefix=$TOOLCHAIN_DIR --static
-  make -j "$(nproc)"
-  sudo make install
-)
-
-echo "$TOOLCHAIN_DIR/bin"
