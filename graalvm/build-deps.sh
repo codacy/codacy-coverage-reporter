d32fbe7956022d535196a28086a2fa6967c30bf5
Leandro Salgado <lmocs98@gmail.com>
2025-01-23 16:45:12 +0000

    add compiler options

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -8,10 +8,26 @@ cd $HOME/.musl
 if [ "$ARCH" == "arm" ];
 then
   echo "https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz"
-  curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz
+  curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz --output musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
 else
   echo "http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz"
   curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
 fi
+
+export CC=$TOOLCHAIN_DIR/bin/gcc
+
+zlib='zlib-1.2.13'
+zlibtargz=$zlib.tar.gz
+curl https://zlib.net/fossils/$zlibtargz --output $zlibtargz
+tar -xf $zlibtargz
+
+(
+  cd $zlib
+  ./configure --prefix=$TOOLCHAIN_DIR --static
+  make -j "$(nproc)"
+  sudo make install
+)
+
+echo "$TOOLCHAIN_DIR/bin"
