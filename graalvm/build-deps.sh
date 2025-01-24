3b158c791a10d74fb2f13e401bf0677b874ff51b
Leandro Salgado <lmocs98@gmail.com>
2025-01-24 11:46:20 +0000

    untar file

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -9,10 +9,12 @@ if [ "$ARCH" == "arm" ];
 then
   echo "https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz"
   curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz --output musl.tgz
+  tar -xf musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
 else
   echo "http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz"
   curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
+  tar -xf musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
 fi
 
