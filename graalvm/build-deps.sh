48c663cac80c9f455b5ab42c68fd6556378fc03d
Leandro Salgado <lmocs98@gmail.com>
2025-01-24 12:57:19 +0000

    add support for linux arm configuration

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -7,10 +7,10 @@ cd $HOME/.musl
 
 if [ "$ARCH" == "arm" ];
 then
-  echo "https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz -output musl.tgz"
-  curl https://more.musl.cc/i686-linux-musl/arm-linux-musleabi-native.tgz --output musl.tgz
+  echo "https://more.musl.cc/i686-linux-musl/aarch64-linux-musl-cross.tgz -output musl.tgz"
+  curl https://more.musl.cc/i686-linux-musl/aarch64-linux-musl-cross.tgz --output musl.tgz
   tar -xf musl.tgz
-  TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
+  TOOLCHAIN_DIR=$HOME/.musl/aarch64-linux-musl-cross
 else
   echo "http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz"
   curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
@@ -22,12 +22,18 @@ export CC=$TOOLCHAIN_DIR/bin/gcc
 
 zlib='zlib-1.2.13'
 zlibtargz=$zlib.tar.gz
+echo "curl https://zlib.net/fossils/\$zlibtargz --output \$zlibtargz"
 curl https://zlib.net/fossils/$zlibtargz --output $zlibtargz
 tar -xf $zlibtargz
 
 (
   cd $zlib
-  ./configure --prefix=$TOOLCHAIN_DIR --static
+  if [ "$ARCH" == "arm" ];
+  then
+    ./configure --prefix=$TOOLCHAIN_DIR --static --build x86_64-pc-linux-gnu --host aarch64-linux-gnu
+  else
+    ./configure --prefix=$TOOLCHAIN_DIR --static
+  fi
   make -j "$(nproc)"
   sudo make install
 )
