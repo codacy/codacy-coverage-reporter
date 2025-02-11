46313f777c660e234555d39f1b5943e5129ac2cf
Leandro Salgado <lmocs98@gmail.com>
2025-02-11 15:09:53 +0000

    change compiler for linux arm

--- graalvm/build-deps.sh
+++ graalvm/build-deps.sh
@@ -9,17 +9,20 @@ uname
 
 if [ "$ARCH" == "arm" ];
 then
-  curl https://musl.cc/arm-linux-musleabi-native.tgz  --output musl.tgz
-  tar -xf musl.tgz
-  TOOLCHAIN_DIR=$HOME/.musl/arm-linux-musleabi-native
+  mkdir -p $HOME/.gcc
+  cd $HOME/.gcc
+  curl https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz  --output gcc.tgz
+  tar -xf gcc.tgz
+  TOOLCHAIN_DIR=$HOME/.gcc/gcc-11.2.0
+  export CC=$TOOLCHAIN_DIR/gcc
 else
   echo "http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz"
   curl http://more.musl.cc/10/x86_64-linux-musl/x86_64-linux-musl-native.tgz --output musl.tgz
   tar -xf musl.tgz
   TOOLCHAIN_DIR=$HOME/.musl/x86_64-linux-musl-native
+  export CC=$TOOLCHAIN_DIR/bin/gcc
 fi
 
-export CC=$TOOLCHAIN_DIR/bin/gcc
 
 zlib='zlib-1.2.13'
 zlibtargz=$zlib.tar.gz
@@ -28,7 +31,7 @@ curl https://zlib.net/fossils/$zlibtargz --output $zlibtargz
 tar -xf $zlibtargz
 
 (
-  $TOOLCHAIN_DIR/bin/gcc --version
+  $CC --version
   cd $zlib
   if [ "$ARCH" == "arm" ];
   then
